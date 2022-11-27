92b3b26e50e9fc82241c9bb538022d524159865f
madiha gul <mmadihagull@gmail.com>
2022-11-27 13:51:55 +0100

    Driver

--- NewMinds/Program.cs
+++ NewMinds/Program.cs
@@ -41,3 +41,55 @@ public class Storm
     }
 
 }
+
+
+
+
+
+
+
+
+public class DriverExam
+{
+    public static void ExecuteExercise(IExercise exercise)
+    {
+        try
+        {
+            exercise.Start();
+
+            exercise.Execute();
+
+            exercise.End();
+        }
+        catch
+        {
+            exercise.MarkNegativePoints();
+        }
+
+        
+    }
+}
+
+public interface IExercise
+{
+    void Start();
+    void Execute();
+    void MarkNegativePoints();
+    void End();
+}
+
+public class Exercise : IExercise
+{
+    public void Start() { Console.WriteLine("Start"); }
+    public void Execute() { Console.WriteLine("Execute"); }
+    public void MarkNegativePoints() { Console.WriteLine("MarkNegativePoints"); }
+    public void End() { Console.WriteLine("End"); }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DriverExam.ExecuteExercise(new Exercise());
+    }
+}
